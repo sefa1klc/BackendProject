2316cc97b6f9c7d26a3417536d06c39521199b84
Sefa <sefakkilic@gmail.com>
2024-10-04 13:42:12 +0300

    deneme

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class FirstProject
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+        }
+    }
+}
